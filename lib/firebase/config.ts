ff9e5af7a6e13c7d661b83ba7c2df0de551af9f7
emre oncu <meoncu@gmail.com>
2026-03-16 16:05:15 +0300

    fix: wire firebase measurement id into client config

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -9,6 +9,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
